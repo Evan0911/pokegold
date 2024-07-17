3c0692d2c3de916d25f5cf2bbbe7fac940118827
Evan <63302524+Evan0911@users.noreply.github.com>
2024-07-17 23:26:17 +0200

    Add 3 TMs for Thunderbolt, Ice Beam and Flamethrower

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -269,6 +269,9 @@ DEF TM01 EQU const_value
 	add_tm FIRE_PUNCH   ; f0
 	add_tm FURY_CUTTER  ; f1
 	add_tm NIGHTMARE    ; f2
+	add_tm THUNDERBOLT  ; f3
+	add_tm ICE_BEAM 		; f4
+	add_tm FLAMETHROWER ; f5
 DEF NUM_TMS EQU __tmhm_value__ - 1
 
 MACRO add_hm
